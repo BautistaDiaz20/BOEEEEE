3aad40d33a023d6ca010bae509446af3fbb4449a
Your Name <you@example.com>
2024-08-13 16:52:59 -0300

    BOEEEEEEEEEEEE BOEEEEEEEEEEEEEE ASI ESTA PROGRAMACION CHICOS?

---
+++ src/main/java/com/mycompany/boeeeeeee/BOEEEEEEE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boeeeeeee;
+
+/**
+ *
+ * @author peret
+ */
+public class BOEEEEEEE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
